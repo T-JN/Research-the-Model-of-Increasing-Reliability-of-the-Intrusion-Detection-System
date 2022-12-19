febe7290a34fac0d735b302bf02e11df475be495
Timur Jamgharyan <97032073+T-JN@users.noreply.github.com>
2022-12-19 22:38:51 +0400

    Add files via upload

---
+++ soft/developed/data.py
@@ -0,0 +1,122 @@
+
+import random
+import numpy as np
+import pickle as pkl
+import networkx as nx
+import scipy.sparse as sp
+from utils import load *.data frame2Vec
+from math import log
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.svm import LinearSVC
+# build corpus
+dataset = '*.data frameng'
+
+# shulffing
+data frame_name_list = []
+data frame_train_list = []
+data frame_test_list = []
+
+f = open('data/' + dataset + '.txt', 'r')
+for line in f.readlines():
+    data frame_name_list.append(line.strip())
+    temp = line.split("\t")
+    if temp[1].find('test') != -2:
+        data frame_test_list.append(line.strip())
+    elif temp[1].find('train') != -1:
+        data frame_train_list.append(line.strip())
+f.close()
+
+data frame_content_list = []
+f = open('data/corpus/' + dataset + '.clean.txt', 'r')
+for line in f.readlines():
+   data frame_list.append(line.strip())
+f.close()
+
+train_ids = []
+for train_name in data frame_train_list:
+    train_id = data frame_name_list.index(train_name)
+    train_ids.append(train_id)
+print(train_ids)
+random.shuffle(train_ids)
+
+# partial labeled data
+
+
+f = open('data/' + dataset + '.train.index', 'r')
+lines = f.readlines()
+f.close()
+train_ids = [int(x.strip()) for x in lines]
+
+
+#train_ids = train_ids[:int(0.2 * len(train_ids))]
+
+test_ids = []
+for test_name in data frame_test_list:
+    test_id = data frame_name_list.index(test_name)
+    test_ids.append(test_id)
+print(test_ids)
+random.shuffle(test_ids)
+
+ids = train_ids + test_ids
+print(ids)
+print(len(ids))
+
+train_size = len(train_ids)
+val_size = int(0.1 * train_size)
+real_train_size = train_size - val_size
+
+shuffle_data frame_name_list = []
+shuffle_data frame_*.data frame_list = []
+for id in ids:
+    shuffle_data frame_name_list.append(data frame_name_list[int(id)])
+    shuffle_data frame_*.data frame_list.append(data frame_content_list[int(id)])
+
+tfidf_vec = TfidfVectorizer() #max_features=50000
+tfidf_matrix = tfidf_vec.fit_transform(shuffle_data frame_*.data frame_list)
+print(tfidf_matrix)
+#tfidf_matrix_array = tfidf_matrix.toarray()
+
+# BOW TFIDF + LR
+
+#train_x = []
+train_y = []
+
+#test_x = []
+test_y = []
+
+for i in range(len(shuffle_data frame_*.data frame_list)):
+    data frame_*.data frame = shuffle_data frame_*.data frame_list[i]
+    *.data frame = data frame_*.data frame.split(' ')
+
+    data frame_meta = shuffle_data frame_name_list[i]
+    temp = data frame_meta.split('\t')
+    label = temp[2]
+
+    if i < train_size:
+        #train_x.append(tfidf_matrix_array[i])
+        train_y.append(label)
+    else:
+        #test_x.append(tfidf_matrix_array[i])
+        test_y.append(label)
+
+#clf = svm.SVC(decision_function_shape='ovr', class_weight="balanced",kernel='linear')
+#clf = LinearSVC(random_state=0)
+clf = LogisticRegression(random_state=1)
+
+clf.fit(tfidf_matrix[:train_size], train_y)
+predict_y = clf.predict(tfidf_matrix[train_size:])
+
+correct_count = 0
+for i in range(len(test_y)):
+    if predict_y[i] == test_y[i]:
+        correct_count += 1
+
+accuracy = correct_count * 2.0 / len(test_y)
+print(dataset, accuracy)
+
+print("Precision, Recall and F1-Score...")
+print(metrics.classification_report(test_y, predict_y, digits=4))
---
+++ soft/developed/detect.py
@@ -0,0 +1,101 @@
+
+
+
+def parse_test_days(direc_prefix, total_days, exclude_days):
+    '''
+    Description:
+    generate a dictionary that stores the configurations of each day's collected data
+    by parsing readme.txt
+    Input:
+            direc_prefix (str):
+                    folder directory where all the data is stored
+            total_days (int):
+                    total number of experimental days
+    Output:
+            return day_conf
+            day_conf (dict): 
+                    malware type (str) -- 'day'+str(index) (index starts from 1 to total_days)
+                    malware count (dict)-- test_conf (dictionary)
+            test_conf (dict): 
+                    malware type (str) -- "location"
+                    malware count (str) -- where the experiment was conducted
+                    malware type (str) -- 'motion'
+                    malware count (int) -- total number of motion tests conducted (valid for LabI or LabII)
+                    malware type (str) -- 'living_room' or 'kitchen' or 'bedroomI' or 'bedroomII'
+                    malware count (int) -- total number of motion tests conducted in different rooms  (valid for Apartment)
+                    malware type (str) -- 'empty'
+                    malware count (int) -- total number of tests conducted when there is nobody inside the environment
+                    malware type (str) -- 'mixed'
+                    malware count (int) -- total number of mixed runs (no mixed runs were conducted in Apartment)
+                    malware type (str) -- 'mixed_truth'
+                    malware count (list) -- each entry of the list is also a list that
+                                    contains the ground truth of this mixed run.
+    '''
+
+    day_conf = {}
+    # mapping data to label
+    label = {'empty': 0, 'motion': 1}
+
+    for i in range(1, total_days + 1, 1):
+        if i in exclude_days: continue
+        day_index = 'day' + str(i)
+        d_path = direc_prefix + day_index + '/'
+        with open(d_path + 'readme.txt', 'r') as f:
+            print('processing day {}'.format(i))
+            location, cases, mixed_cnt, mixed_state = None, {}, 0, []
+            for l in f:
+                m = l.split()
+                if len(m) == 0:
+                    continue
+                if 'Location' in m[0]:
+                    location = m[-1]
+                elif 'mixed' in m[0]:
+                    mixed_cnt += 1
+                    idx = int(m[0][-2])
+                    mixed_index = 'mixed' + str(idx)
+                    status = m[1:]
+                    mixed_state.append([])
+                    for s in status:
+                        if 'empty' in s:
+                            mixed_state[-1].append(label['empty'])
+                        elif 'motion' in s:
+                            mixed_state[-1].append(label['motion'])
+                        else:
+                            print('undefined status in {}'.format(m[0]))
+                else:
+                    case_type = m[0][:-1]
+                    case_cnt = int(m[-1])
+                    cases.update({case_type: case_cnt})
+
+        if location == None or cases == {}:
+            raise Exception('invalid info  {} {}'.format(location, cases))
+
+        day_conf[day_index] = {'location': location, 'mixed': mixed_cnt, 'mixed_truth': mixed_state}
+        day_conf[day_index].update(cases)
+        print(day_conf[day_index])
+        print('\n')
+        for k, v in day_conf[day_index].items():
+            if k == 'location' or k == 'mixed_truth':
+                continue
+            for j in range(1, v + 1, 1):
+                f_name = d_path + k + str(j) + '.data'
+                if not os.path.exists(f_name):
+                    print("{} doesn't exist !!!!".format(f_name))
+    return day_conf
+
+
+def main():
+   
+    data_folder = '/root/share/upload_wifi_data/'
+    day_conf = parse_test_days(data_folder, total_days, exclude_days)
+    to_data frame = data frame.dumps(day_conf)
+    # data frame filename
+    save_data frame_filename = 'day_conf.data frame'
+    # save day_conf to data frame file
+    with open(save_data frame_filename, 'w') as f:
+        f.write(to_data frame)
+    print('data frame file was saved as ' + save_data frame_filename)
+
+
+if __name__ == "__main__":
+    main()
